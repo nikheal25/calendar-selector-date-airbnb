880ccc9d48754b2168f4be88a5cac26375cb845e
Nick <nikheal25@gmail.com>
2020-08-25 07:55:33 +1000

    New calendar is working

--- horizontalCalendar/DetailViewController.swift
+++ horizontalCalendar/DetailViewController.swift
@@ -11,32 +11,70 @@ import HorizonCalendar
 
 class DetailViewController: UIViewController {
     @IBOutlet weak var insideCalendarView: UIView!
-//    private var selectedDay: Day?
-//
-//
-//
-//    private let monthsLayout: MonthsLayout
-//
-//    private lazy var calendar = Calendar(identifier: .gregorian)
-//    private lazy var dayDateFormatter: DateFormatter = {
-//      let dateFormatter = DateFormatter()
-//      dateFormatter.calendar = calendar
-//      dateFormatter.dateFormat = DateFormatter.dateFormat(
-//        fromTemplate: "EEEE, MMM d, yyyy",
-//        options: 0,
-//        locale: calendar.locale ?? Locale.current)
-//      return dateFormatter
-//    }()
-
+    //    private var selectedDay: Day?
+    //
+    //
+    //
+    //    private let monthsLayout: MonthsLayout
+    //
+    //    private lazy var calendar = Calendar(identifier: .gregorian)
+    //    private lazy var dayDateFormatter: DateFormatter = {
+    //      let dateFormatter = DateFormatter()
+    //      dateFormatter.calendar = calendar
+    //      dateFormatter.dateFormat = DateFormatter.dateFormat(
+    //        fromTemplate: "EEEE, MMM d, yyyy",
+    //        options: 0,
+    //        locale: calendar.locale ?? Locale.current)
+    //      return dateFormatter
+    //    }()
+    private enum CalendarSelection {
+        case singleDay(Day)
+        case dayRange(DayRange)
+    }
+    private var calendarSelection: CalendarSelection?
+    private lazy var calendarView = CalendarView(initialContent: makeContent())
+    private lazy var calendar = Calendar(identifier: .gregorian)
+    private lazy var dayDateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.calendar = calendar
+        dateFormatter.dateFormat = DateFormatter.dateFormat(
+            fromTemplate: "EEEE, MMM d, yyyy",
+            options: 0,
+            locale: calendar.locale ?? Locale.current)
+        return dateFormatter
+    }()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        let calendarView = CalendarView(initialContent: makeContent())
+        
+        
+        
+        calendarView.daySelectionHandler = { [weak self] day in
+            guard let self = self else { return }
+            
+            switch self.calendarSelection {
+            case .singleDay(let selectedDay):
+                if day > selectedDay {
+                    self.calendarSelection = .dayRange(selectedDay...day)
+                } else {
+                    self.calendarSelection = .singleDay(day)
+                }
+            case .none, .dayRange:
+                self.calendarSelection = .singleDay(day)
+            }
+            
+            self.calendarView.setContent(self.makeContent())
+        }
+        
         
         self.insideCalendarView.addSubview(calendarView)
         
         calendarView.translatesAutoresizingMaskIntoConstraints = false
+        
+        
         
         NSLayoutConstraint.activate([
             calendarView.leadingAnchor.constraint(equalTo: insideCalendarView.layoutMarginsGuide.leadingAnchor),
@@ -48,20 +86,61 @@ class DetailViewController: UIViewController {
     }
     
     private func makeContent() -> CalendarViewContent {
-//        let selectedDay = self.selectedDay
+        //        let selectedDay = self.selectedDay
         
-        let calendar = Calendar(identifier: .gregorian)
+//        let calendar = Calendar(identifier: .gregorian)
         
         let startDate = calendar.date(from: DateComponents(year: 2020, month: 01, day: 01))!
         let endDate = calendar.date(from: DateComponents(year: 2021, month: 12, day: 31))!
+        
+        let calendarSelection = self.calendarSelection
+        let dateRanges: Set<ClosedRange<Date>>
+        if
+            case .dayRange(let dayRange) = calendarSelection,
+            let lowerBound = calendar.date(from: dayRange.lowerBound.components),
+            let upperBound = calendar.date(from: dayRange.upperBound.components)
+        {
+            dateRanges = [lowerBound...upperBound]
+        } else {
+            dateRanges = []
+        }
+        
         
         return CalendarViewContent(
             calendar: calendar,
             visibleDateRange: startDate...endDate,
             monthsLayout: .vertical(options: VerticalMonthsLayoutOptions()))
+            .withDayItemProvider { day in
+                let isSelected: Bool
+                switch calendarSelection {
+                case .singleDay(let selectedDay):
+                    isSelected = day == selectedDay
+                case .dayRange(let selectedDayRange):
+                    isSelected = day == selectedDayRange.lowerBound || day == selectedDayRange.upperBound
+                case .none:
+                    isSelected = false
+                }
+                
+                return CalendarItem<DayView, Day>(
+                    viewModel: day,
+                    styleID: isSelected ? "Selected" : "Default",
+                    buildView: { DayView(isSelectedStyle: isSelected) },
+                    updateViewModel: { [weak self] dayView, day in
+                        dayView.dayText = "\(day.day)"
+                        
+                        if let date = self?.calendar.date(from: day.components) {
+                            dayView.dayAccessibilityText = self?.dayDateFormatter.string(from: date)
+                        } else {
+                            dayView.dayAccessibilityText = nil
+                        }
+                    },
+                    updateHighlightState: { dayView, isHighlighted in
+                        dayView.isHighlighted = isHighlighted
+                })
+        }
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        DayRangeSelectionDemoViewController.init(monthsLayout: .horizontal(monthWidth: min(min(view.bounds.width, view.bounds.height) - 64, 512)))
+        
     }
 }
